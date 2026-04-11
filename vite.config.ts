fd238ae4f1f79b806c6a1e4a0a7a17f0dde2c171
Jayden <jaydenk3@gmail.com>
2026-04-11 09:36:03 +0200

    feat: initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 
 import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import path from "path"
 
 const config = defineConfig({
   plugins: [
@@ -19,6 +20,11 @@ const config = defineConfig({
       },
     }),
   ],
+  resolve: {
+    alias: {
+      '#': path.resolve(__dirname, './src'),
+    },
+  },
 })
 
 export default config
